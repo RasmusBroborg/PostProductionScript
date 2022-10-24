9230bae1d689d4c24c4ecfc216610d8e33a1a49c
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-10-24 16:56:37 +0200

    rename methods in Script.cs for clarity.

--- src/PostProductionScript/Script.cs
+++ src/PostProductionScript/Script.cs
@@ -17,10 +17,12 @@ namespace PostProductionScript
     /// Represents the script title.
     /// </summary>
     public string Title { get; set; } = "";
+
     /// <summary>
     /// Represents the general spoken language of the script.
     /// </summary>
     public string Language { get; set; } = "";
+
     /// <summary>
     /// Represents the general description of the script.
     /// </summary>
@@ -30,54 +32,64 @@ namespace PostProductionScript
     /// Represents the script lines.
     /// </summary>
     public List<IScriptLine> Lines { get; } = new List<IScriptLine>();
+
     /// <summary>
-    /// Adds hours to all script lines.
+    /// Offsets the hours of all script lines by a given amount.
     /// </summary>
     /// <param name="hours">The number of hours to add to all script lines.</param>
-    public void AddHours(int hours)
+    public void OffsetHours(int hours)
     {
       throw new NotImplementedException();
     }
+
     /// <summary>
-    /// Adds minutes to all script lines.
+    /// Offsets the minutes of all script lines by a given amount.
     /// </summary>
     /// <param name="minutes">The number of minutes to add to all script lines</param>
-    public void AddMinutes(int minutes)
+    public void OffsetMinutes(int minutes)
     {
       throw new NotImplementedException();
     }
+
     /// <summary>
-    /// Adds seconds to all script lines.
+    /// Offsets the seconds of all script lines by a given amount.
     /// </summary>
     /// <param name="seconds">The number of seconds to add to all script lines</param>
-    public void AddSeconds(int seconds)
+    public void OffsetSeconds(int seconds)
     {
       throw new NotImplementedException();
     }
+
     /// <summary>
-    /// Adds frames to all script lines.
+    /// Offsets the frames of all script lines by a given amount.
     /// </summary>
     /// <param name="frames">The number of frames to add to all script lines</param>
-    public void AddFrames(int frames)
+    public void OffsetFrames(int frames)
     {
       throw new NotImplementedException();
     }
+
     /// <summary>
     /// Changes the scripts framerate.
     /// </summary>
     /// <param name="framerate">The script target framerate.</param>
     /// <param name="convertExistingTimecodes">Determines if all existing timecodes should be converted to the target framerate. 
     /// <br/> True set as default behaviour.</param>
-    public void ChangeFramerate(Framerate framerate, bool convertExistingTimecodes = true)
+    public void ChangeScriptFramerate(Framerate framerate, bool convertExistingTimecodes = true)
     {
       throw new NotImplementedException();
     }
 
     // TODO: Fix line classes. Must insert a new line and update the line numbers of all other lines.
-    public void InsertLine()
+    /// <summary>
+    /// Adds a line from the script.
+    /// </summary>
+    /// <param name="lineNumber">The line number which should be removed.</param>
+    public void InsertLine(int lineNumber)
     {
       throw new NotImplementedException();
     }
+
     /// <summary>
     /// Removes a line from the script.
     /// </summary>
