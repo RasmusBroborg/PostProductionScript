5b881872864a26d43d0690658df02a7f7e825a11
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-11-12 10:38:17 +0100

    Refactor ConvertSrtToAsBroadcastScript test to increase test coverage.

--- tests/PostProductionScript_Test/Managers/SubtitleManager_Test.cs
+++ tests/PostProductionScript_Test/Managers/SubtitleManager_Test.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using PostProductionScript.Managers;
-
-using DotnetTimecode;
 using DotnetTimecode.Enums;
-
-using PostProductionScript.Models.LineModels;
 
 using Xunit;
 using FluentAssertions;
@@ -17,41 +9,43 @@ namespace PostProductionScript_Test.Managers
 {
   public class SubtitleManager_Test
   {
-    [Theory] // Arrange
-    [InlineData(@$"
+    [Fact]
+    public void ConvertSrtToAsBroadcastScript_ValidInputs_Successful()
+    {
+      // Arrange
+      var framerate = Framerate.fps24;
+      var validSrt = @$"
 1
-00:00:01,500 --> 00:00:07,500
+00:00:00,000 --> 00:00:01,500
 Lorem ipsum dolor sit amet, 
 consectetur adipiscing elit.
 
 2
-00:00:08,500 --> 00:00:10,000
+00:00:01,000 --> 00:00:02,500
 Sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua. 
 
 3
-00:00:10,000 --> 00:00:13,500
+00:00:02,000 --> 00:00:03,500
 Justo donec enim diam vulputate. 
 
 4
-00:00:13,500 --> 00:00:17,000
+00:00:03,000 --> 00:00:04,500
 Id cursus metus aliquam eleifend 
 mi in nulla.
 
 5
-00:00:17,000 --> 00:00:21,000
+00:00:04,000 --> 00:00:05,500
 Ornare aenean euismod elementum nisi. 
 Sit amet mattis vulputate enim.
 
 6
-00:00:21,000 --> 00:00:26,000
+00:00:05,000 --> 00:00:06,500
 Fermentum dui faucibus in ornare quam
 viverra. Elit eget gravida cum sociis 
 natoque penatibus et magnis dis.
-", Framerate.fps25
-      )]
-    public void ConvertSrtToAsBroadcastScript_ValidInputs_Successful(string validSrt, Framerate framerate)
-    {
+";
+
       // Act
       var result = SubtitleManager.ConvertSrtToAsBroadcastScript(validSrt, framerate);
 
@@ -61,6 +55,10 @@ natoque penatibus et magnis dis.
       for (int i = 0; i < result.Lines.Count(); i++)
       {
         result.Lines.ElementAt(i).LineNumber.Should().Be(i+1);
+        result.Lines.ElementAt(i).TimecodeIn!.Second.Should().Be(i);
+        result.Lines.ElementAt(i).TimecodeIn!.Frame.Should().Be(0);
+        result.Lines.ElementAt(i).TimecodeOut!.Second.Should().Be(i+1);
+        result.Lines.ElementAt(i).TimecodeOut!.Frame.Should().Be(12); // 24fps*0.500
       }
     }
   }
