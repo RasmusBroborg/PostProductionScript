7c4f366449cb7ab8c603e9cd0dab05d6bbfde6ae
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-11-14 13:07:55 +0100

    Apply fallback commit.

--- src/PostProductionScript.UI/ViewModels/MainWindowViewModel.cs
+++ src/PostProductionScript.UI/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,41 @@
-﻿namespace PostProductionScript.UI.ViewModels
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using JetBrains.Annotations;
+
+using PostProductionScript.Models.ScriptModels;
+
+namespace PostProductionScript.UI.ViewModels
 {
-  public class MainWindowViewModel : ViewModelBase
+  public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
   {
+    private TimecodeScript _script = new TimecodeScript();
+
+    public TimecodeScript Script
+    {
+      get => _script;
+      set
+      {
+        if (Script == value) return;
+
+        _script = value;
+        OnPropertyChanged(nameof(Script));
+      }
+    }
+
     public string Greeting => "Welcome to Avalonia!";
+
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
   }
 }
